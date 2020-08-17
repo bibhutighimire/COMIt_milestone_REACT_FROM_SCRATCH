89ed92e93a94ddce182f8132f70c96e365a29bb5
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-16 23:47:34 -0600

    onclick event and update on change event created; onclick does not work; filter login coded but doesnot work

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -4,22 +4,67 @@ import "../css/Home.css";
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import recipeData from "../data";
+import { v4 as uuid } from "uuid";
 
 class SearchPage extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: "",
+      recipeList: [],
+    };
+  } 
+  updateItem(key, value) {
+    this.setState({ [key]: value });
+  }
 
+    submitHandler = (event) => {
+     
+      event.preventDefault();
+      let newArray = [];
+      recipeData.map((recipe) =>
+      recipe.title
+          .toLowerCase()
+          .indexOf(this.state.userSearch.toLowerCase()) !== -1
+          ? newArray.push(recipe)
+          : null
+      )
+   
+
+    this.setState({ recipeList: [newArray] });
+    this.updateItem("userSearch", "");
+  };
     
-    return (
-      <div className="recGalleryContainer">
-      <Header titleHeader="Contact Us"/>
 
+  render() {
+    
+  
+
+return (
+  
+ 
+      <div className="recGalleryContainer">
+      <Header titleHeader="Search Your Fav Recipe here!"/>
+      console.log(recipeData);
+  console.log("recipeData");
         <p className="pHome">This is a search page. Please type the title of food that you want to check out and we will try to provide you with details</p> 
-      <label htmlFor="searchInput">
-       <input type="text" className="searchInput" placeholder="Type the title of food and we will display it's full recipe!" />
+        <form onSubmit={this.submitHandler}>
+     
+       <input className="searchInput" type="text"  value={this.state.searchInput} onChange={(event) =>this.updateItem("searchInput",event.target.value)}  placeholder="Type the title of food and we will display it's full recipe!" />
        <button type="submit" id="btnSubmit" >Search</button>
-       </label>
+      
+       </form>
+
+       {this.state.recipeList.map((recipe) =>
+          (
+            <div className="recipeCard" key={uuid()}>
+              <h2>{recipe.title ? recipe.title : <>Not Available</>}</h2>
+       </div>
+          ))}
         <Footer />
       </div>
+      
           );
           }
         }
