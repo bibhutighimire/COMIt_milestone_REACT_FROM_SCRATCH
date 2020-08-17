cd79bb974f425e50481b850e422b9a82d7d46420
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-16 20:57:23 -0600

    search form and button added; onclick and change state work in progres

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,4 +1,4 @@
-
+import "../css/searchPage.css";
 import "../css/styles.css";
 import "../css/Home.css";
 import React from "react";
@@ -7,11 +7,17 @@ import Header from "./Header";
 
 class SearchPage extends React.Component {
   render() {
+
+    
     return (
       <div className="recGalleryContainer">
       <Header titleHeader="Contact Us"/>
+
         <p className="pHome">This is a search page. Please type the title of food that you want to check out and we will try to provide you with details</p> 
-       
+      <label htmlFor="searchInput">
+       <input type="text" className="searchInput" placeholder="Type the title of food and we will display it's full recipe!" />
+       <button type="submit" id="btnSubmit" >Search</button>
+       </label>
         <Footer />
       </div>
           );
