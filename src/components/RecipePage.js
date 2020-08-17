7f1aa513097fd49bb9d1790b40737288ce1d73b5
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-16 18:52:25 -0600

    mapping the data issue but managed to get some data into UI; need to fix why img link is coming instead of real image

--- src/components/RecipePage.js
+++ src/components/RecipePage.js
@@ -5,94 +5,32 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import recipeData from "../data";
-import { v4 as uuid } from "uuid";
-import { faHeart, faHamburger } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class RecipePage extends React.Component {
 
-render() {
-    return (
-      <div className="recGalleryContainer">
-      <Header titleHeader="View all our Recipe Here"/>
-        <p className="pHome">Welcome to Recipe Page. Bwloe are the recipe you can view and also click on hearn icon beside it to move it to your Faves page to refer back later.</p> 
-        <div className="favContainer">
+    render() {
+        return (
+            <div className="recGalleryContainer">
+                <Header titleHeader="View all our Recipe Here" />
+                <p className="pHome">Welcome to Recipe Page. Bwloe are the recipe you can view and also click on hearn icon beside it to move it to your Faves page to refer back later.</p>
+                <div className="favContainer">
+                    <ul>
+                        {recipeData.map((recipes) => 
+                        <li>
+                    <figure>
+{recipes.image}
 
-            {/* <div key={uuid()}> */}
-              <h2>{recipeData.title}</h2>
-             
-              <figure>
-                <img
-                  src={recipeData.image ? recipeData.image : ""}
-                  alt="Food Recipe To See"
-                />
-                <figcaption>
-                  <ul>
-                    <li className="label">
-                      {recipeData.summary
-                        ? recipeData.summary.replace(/(<([^>]+)>)/gi, "")
-                        : ""}
+                    </figure>
                     </li>
-                    {/* <li>
-                      <span className="topicHeader">Ingredients: </span>
-                      <span className="topicP">
-                        {indrecipe.analyzedInstructions ? (
-                          indrecipe.analyzedInstructions.map(
-                            (analyzedInstruction) =>
-                              analyzedInstruction.steps.map((stepsInd) =>
-                                stepsInd.ingredients.map((ingredient) => (
-                                  <div key={uuid()}>
-                                    {ingredient.name ? (
-                                      ingredient.name
-                                    ) : (
-                                      <>Not available</>
-                                    )}
-                                    ,{" "}
-                                  </div>
-                                ))
-                              )
-                          )
-                        ) : (
-                          <>Not Avaialble</>
-                        )}
-                      </span>
-                    </li> */}
-                    <li>
-                      <span className="topicHeader">Total Time: </span>
-                      <span className="topicP">
-                        {recipeData.readyInMinutes ? (
-                          <>{recipeData.readyInMinutes} minutes</>
-                        ) : (
-                          <>Not Available</>
-                        )}
-                      </span>
-                    </li>
-                    <li>
-                      <span className="topicHeader">Influenced By: </span>
-                      <span className="topicP">
-                        <a
-                          href={recipeData.sourceUrl ? recipeData.sourceUrl : ""}
-                          rel="noopener noreferrer"
-                          target="_blank"
-                        >
-                          Click Here to check out their URL
-                        </a>
-                      </span>
-                    </li>
-                  </ul>
-                </figcaption>
-              </figure>
-            {/* </div> */}
-          ))
-        ) : (
-          <>
-            (<h2>" No Favorited recipes yet"</h2>)
-          </>
-        )}
-</div>
-        <Footer />
-      </div>
-          );
-          }
-        }
+                    )}
+                    </ul>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
 export default (RecipePage);
+// {recipeData.map((recipes)=>
+//     recipes.title ? (recipes.title):null
+//  )}
