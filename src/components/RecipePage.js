1ca8dddddeaaf1e85f2c9a0fcca80f300090d75c
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-16 19:08:21 -0600

    total time, ingredients,influenced by added; css needs fix

--- src/components/RecipePage.js
+++ src/components/RecipePage.js
@@ -5,6 +5,7 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import recipeData from "../data";
+import { v4 as uuid } from "uuid";
 
 class RecipePage extends React.Component {
 
@@ -22,6 +23,62 @@ class RecipePage extends React.Component {
 <img src={recipes.image} alt="food" />
 
                     </figure>
+                    <figcaption>
+                    <ul>
+                    <li className="label">{recipes.summary
+                        ? recipes.summary.replace(/(<([^>]+)>)/gi, "")
+                        : ""} </li>
+
+<li>
+                      <span className="topicHeader">Ingredients: </span>
+                      <span className="topicP">
+                        {recipes.analyzedInstructions ? (
+                          recipes.analyzedInstructions.map(
+                            (analyzedInstruction) =>
+                              analyzedInstruction.steps.map((stepsInd) =>
+                                stepsInd.ingredients.map((ingredient) => (
+                                  <div key={uuid()}>
+                                    {ingredient.name ? (
+                                      ingredient.name
+                                    ) : (
+                                      <>Not available</>
+                                    )}
+                                    ,{" "}
+                                  </div>
+                                ))
+                              )
+                          )
+                        ) : (
+                          <>Not Avaialble</>
+                        )}
+                      </span>
+                    </li>
+
+                    <li>
+                      <span className="topicHeader">Total Time: </span>
+                      <span className="topicP">
+                        {recipes.readyInMinutes ? (
+                          <>{recipes.readyInMinutes} minutes</>
+                        ) : (
+                          <>Not Available</>
+                        )}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="topicHeader">Influenced By: </span>
+                      <span className="topicP">
+                        <a
+                          href={recipes.sourceUrl ? recipes.sourceUrl : ""}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          Click Here to check out their URL
+                        </a>
+                      </span>
+                    </li>
+
+                    </ul>
+                    </figcaption>
                     </li>
                     )}
                     </ul>
